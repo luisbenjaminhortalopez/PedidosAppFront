5467e23a27a5b60e62809b4119c9f45e2d953b74
mcb75851@udelasallebajio.edu.mx <115039291+Mauriciojcb@users.noreply.github.com>
2024-12-11 20:50:26 -0600

    prueba3

--- lib/main.dart
+++ lib/main.dart
@@ -52,3 +52,221 @@ class MainApp extends StatelessWidget {
   }
 }
 
+import 'package:flutter/material.dart';
+import 'package:pedidos_app/screens/pedidosScreen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+      title: 'Registro de Pedido',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        textTheme:const TextTheme(
+          //bodyText2: TextStyle(fontSize: 16.0, color: Colors.black87),
+        ),
+      ),
+      home:  const PedidosScreen(),
+    );
+  }
+}
+
+
+
+import 'package:flutter/material.dart';
+import 'package:pedidos_app/screens/pedidosScreen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+      title: 'Registro de Pedido',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        textTheme:const TextTheme(
+          //bodyText2: TextStyle(fontSize: 16.0, color: Colors.black87),
+        ),
+      ),
+      home:  const PedidosScreen(),
+    );
+  }
+}
+
+import 'package:flutter/material.dart';
+import 'package:pedidos_app/screens/pedidosScreen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+      title: 'Registro de Pedido',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        textTheme:const TextTheme(
+          //bodyText2: TextStyle(fontSize: 16.0, color: Colors.black87),
+        ),
+      ),
+      home:  const PedidosScreen(),
+    );
+  }
+}
+
+
+
+import 'package:flutter/material.dart';
+import 'package:pedidos_app/screens/pedidosScreen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+      title: 'Registro de Pedido',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        textTheme:const TextTheme(
+          //bodyText2: TextStyle(fontSize: 16.0, color: Colors.black87),
+        ),
+      ),
+      home:  const PedidosScreen(),
+    );
+  }
+}
+
+import 'package:flutter/material.dart';
+import 'package:pedidos_app/screens/pedidosScreen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+      title: 'Registro de Pedido',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        textTheme:const TextTheme(
+          //bodyText2: TextStyle(fontSize: 16.0, color: Colors.black87),
+        ),
+      ),
+      home:  const PedidosScreen(),
+    );
+  }
+}
+
+
+
+import 'package:flutter/material.dart';
+import 'package:pedidos_app/screens/pedidosScreen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+      title: 'Registro de Pedido',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        textTheme:const TextTheme(
+          //bodyText2: TextStyle(fontSize: 16.0, color: Colors.black87),
+        ),
+      ),
+      home:  const PedidosScreen(),
+    );
+  }
+}
+
+import 'package:flutter/material.dart';
+import 'package:pedidos_app/screens/pedidosScreen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+      title: 'Registro de Pedido',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        textTheme:const TextTheme(
+          //bodyText2: TextStyle(fontSize: 16.0, color: Colors.black87),
+        ),
+      ),
+      home:  const PedidosScreen(),
+    );
+  }
+}
+
+
+
+import 'package:flutter/material.dart';
+import 'package:pedidos_app/screens/pedidosScreen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+      title: 'Registro de Pedido',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        textTheme:const TextTheme(
+          //bodyText2: TextStyle(fontSize: 16.0, color: Colors.black87),
+        ),
+      ),
+      home:  const PedidosScreen(),
+    );
+  }
+}
+
+
+
